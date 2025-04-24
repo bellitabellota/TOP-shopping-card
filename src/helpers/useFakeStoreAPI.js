9e715d1f9a1e698c02efeda4284c6c319fe0866b
bellitabellota <136174454+bellitabellota@users.noreply.github.com>
2025-04-24 10:25:18 +0100

    Wrap products fetching in a custom hook and extract into separate file

---
+++ src/helpers/useFakeStoreAPI.js
@@ -0,0 +1,28 @@
+import { useState, useEffect } from "react";
+
+const useFakeStoreAPI = () => {
+  const [products, setProducts] = useState(null);
+  const [error, setError] = useState(null);
+  const [loading, setLoading] = useState(true);
+
+  useEffect(() => {
+    fetch('https://fakestoreapi.com/products', { mode: "cors" })
+    .then(response => {
+      if(!response.ok) {
+        throw new Error("server error")
+      }
+      return response.json()})
+    .then(data => {
+      setProducts(data)}
+    )
+    .catch(error => {
+      setError(error)
+    })
+    .finally(()=> setLoading(false))
+    
+  }, []);
+
+  return { products, error, loading };
+}
+
+export default useFakeStoreAPI;
