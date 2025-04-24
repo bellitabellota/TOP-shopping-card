f2d84bbb7a022b97bbaec45a58e042e6b33711a7
bellitabellota <136174454+bellitabellota@users.noreply.github.com>
2025-04-24 10:15:28 +0100

    Fetch products from FakeStore API in useEffect hook

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,32 @@
 import { RouterProvider } from "react-router-dom";
-import router from "./routes"
+import router from "./routes";
+import { useState, useEffect } from "react";
 
 function App() {
+  const [products, setProducts] = useState(null);
+  const [error, setError] = useState(null);
+  const [loading, setLoading] = useState(true);
+
+  useEffect(() => {
+    fetch('https://fakestoreapi.com/products', { mode: "cors" })
+    .then(response => {
+      if(!response.ok) {
+        throw new Error("server error")
+      }
+      return response.json()})
+    .then(data => {
+      setProducts(data)}
+    )
+    .catch(error => {
+      setError(error)
+    })
+    .finally(()=> setLoading(false))
+    
+  }, []);
+
+  if(loading) return <p>Products loading...</p>;
+  if(error) return <p>{error.message}</p>;
+
   return (
     <RouterProvider router={router} />
   )
