808bd3eedfe5af06baa8ed3e0698ec20a7d22d89
bellitabellota <136174454+bellitabellota@users.noreply.github.com>
2025-04-25 11:00:02 +0100

    Render product list in Home component

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,12 +1,23 @@
 import NavBar from "../sections/NavBar";
 
 function Home({products}) {
+  const productCards = products.map((product) => {
+    return <div className="product-card" key={product.id}>
+              <img src={product.image} alt={product.title} />
+              <div className="product-text">
+                <h2>{product.title}</h2>
+                <p>{product.description}</p>
+                <p>{product.price}</p>
+              </div>
+            </div>
+  })
+
   return (
     <>
       <NavBar />
       <main>
         All Items
-        {products[0].title}
+       {productCards}
       </main>
     </>
   )
