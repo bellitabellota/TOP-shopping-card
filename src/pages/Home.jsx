351a5773a2901462cd82d9ec2b80c84fbc218c46
bellitabellota <136174454+bellitabellota@users.noreply.github.com>
2025-04-26 09:27:31 +0100

    Style Home component

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,13 +1,16 @@
 import NavBar from "../sections/NavBar";
+import styles from "./Home.module.css"
 
 function Home({products}) {
   const productCards = products.map((product) => {
-    return <div className="product-card" key={product.id}>
+    return <div className={styles.productCard} key={product.id}>
               <img src={product.image} alt={product.title} />
-              <div className="product-text">
-                <h2>{product.title}</h2>
-                <p>{product.description}</p>
-                <p>{product.price}</p>
+              <div className={styles.productText}>
+                <div>
+                  <h2>{product.title}</h2>
+                  <p className={styles.productDescription}>{product.description}</p>
+                </div>
+                <p className={styles.productPrice}>{product.price} EUR</p>
               </div>
             </div>
   })
@@ -15,8 +18,8 @@ function Home({products}) {
   return (
     <>
       <NavBar />
+      <h1>All Products</h1>
       <main>
-        All Items
        {productCards}
       </main>
     </>
