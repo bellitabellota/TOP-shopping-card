a0d96301764cd6fac2051c5bf323fac74e27df1c
bellitabellota <136174454+bellitabellota@users.noreply.github.com>
2025-04-30 15:17:19 +0100

    Add Snapshot test for the case that loading is true

--- test/App.test.jsx
+++ test/App.test.jsx
@@ -1,10 +1,16 @@
 import { render, screen } from "@testing-library/react";
-
 import App from "../src/App"
-import { describe, it } from "vitest";
+import { describe, it, vi, expect } from "vitest";
+
+vi.mock("../src/helpers/useFakeStoreAPI.js", () => ({
+  default: vi.fn(() => {
+    return {product:[], error:null, loading:true }
+  }),
+}));
 
 describe("App", ()=>{
-  it("renders", () => {
-    render(<App />)
+  it("renders 'Products loading... when loading is true", () => {
+    const {container} = render(<App />)
+    expect(container).toMatchSnapshot();
   })
 })
