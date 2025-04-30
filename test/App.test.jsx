e64afec2386492550e0eae3595e4d43b162d3ebd
bellitabellota <136174454+bellitabellota@users.noreply.github.com>
2025-04-30 16:45:53 +0100

    Add test for the case useFakeStoreAPI returns an Error

--- test/App.test.jsx
+++ test/App.test.jsx
@@ -1,16 +1,22 @@
 import { render, screen } from "@testing-library/react";
 import App from "../src/App"
 import { describe, it, vi, expect } from "vitest";
+import useFakeStoreAPI from "../src/helpers/useFakeStoreAPI"; // this needs to be explicitly imported as I modify the mockReturnValue inside my test
 
-vi.mock("../src/helpers/useFakeStoreAPI.js", () => ({
-  default: vi.fn(() => {
-    return {product:[], error:null, loading:true }
-  }),
+vi.mock("../src/helpers/useFakeStoreAPI", () => ({
+  default: vi.fn()
 }));
 
-describe("App", ()=>{
+describe("App", () => {
   it("renders 'Products loading... when loading is true", () => {
+    useFakeStoreAPI.mockReturnValue({ products: [], error: null, loading: true });
     const {container} = render(<App />)
     expect(container).toMatchSnapshot();
+  })
+
+  it("renders error message if useFakeStoreAPI returned an Error", () => {
+    useFakeStoreAPI.mockReturnValue({ products: [], error: new Error("server error"), loading: false });
+    render(<App />)
+    expect(screen.getByText("server error")).toBeInTheDocument();
   })
 })
